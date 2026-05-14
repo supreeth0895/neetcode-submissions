ab7733d95d5fe579b5d1caabaaa54831d2540c64
supreeth0895 <supreeth0895@gmail.com>
2026-05-13 23:26:13 -0700

    Add: decode-ways - submission-5

---
+++ Data Structures & Algorithms/decode-ways/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        my_map = {}
+        def backtrack(cur_idx):
+            if cur_idx in my_map:
+                return my_map[cur_idx]
+            if cur_idx == len(s):
+                return 1
+            if s[cur_idx] == "0":
+                return 0
+
+            ways1 = backtrack(cur_idx+1)
+            ways2 = 0
+            if cur_idx < len(s)-1:
+                if (s[cur_idx] == "1") or (s[cur_idx] == "2" and s[cur_idx+1] <= "6"):
+                    ways2 = backtrack(cur_idx+2)
+            my_map[cur_idx] = ways1+ways2
+            return ways1+ways2
+            
+        return backtrack(0)
