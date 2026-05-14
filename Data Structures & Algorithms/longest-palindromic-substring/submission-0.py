5cdc73f0d54db0398ef3596fa68b39551a63fed8
supreeth0895 <supreeth0895@gmail.com>
2026-05-13 21:11:05 -0700

    Add: longest-palindromic-substring - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        max_len = 0
+        max_str = ""
+        def backtrack(left_idx, right_idx):
+            nonlocal max_len
+            nonlocal max_str
+            if left_idx < 0 or right_idx >= len(s) or s[left_idx] != s[right_idx]:
+                cur_len = right_idx - left_idx -1
+                if cur_len > max_len:
+                    max_len = cur_len
+                    max_str = s[left_idx+1:right_idx]
+                return
+            if s[left_idx] == s[right_idx]:
+                left_idx = left_idx -1
+                right_idx = right_idx + 1
+                backtrack(left_idx, right_idx)
+
+        for i in range(0,len(s)):
+            backtrack(i,i)
+            if i < len(s)-1:
+                if s[i] == s[i+1]:
+                    backtrack(i,i+1)
+        
+        return max_str
+
+        
