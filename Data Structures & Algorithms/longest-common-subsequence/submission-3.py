c12a56554a93a3c54c092a2dfd6b86ae7d75592a
supreeth0895 <supreeth0895@gmail.com>
2026-05-17 15:10:52 -0700

    Add: longest-common-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        def backtrack(cur_idx1,cur_idx2):
+            if cur_idx1 == len(text1) or cur_idx2 == len(text2):
+                return 0
+            if (cur_idx1,cur_idx2) in my_map:
+                return my_map[(cur_idx1,cur_idx2)]
+            option1 = 0
+            if text1[cur_idx1] == text2[cur_idx2]:
+                option1 = 1 + backtrack(cur_idx1+1,cur_idx2+1)
+                my_map[(cur_idx1,cur_idx2)] = option1
+            else:
+                option2 = backtrack(cur_idx1+1,cur_idx2)
+                option3 = backtrack(cur_idx1,cur_idx2+1)
+                my_map[(cur_idx1,cur_idx2)] = max(option2, option3)
+            return my_map[(cur_idx1,cur_idx2)]
+        
+        my_map = {}
+        return backtrack(0,0)
