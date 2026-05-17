897caa4a9309fce82dcc175d20499d4314a3074e
supreeth0895 <supreeth0895@gmail.com>
2026-05-17 10:49:34 -0700

    Add: word-ladder - submission-3

---
+++ Data Structures & Algorithms/word-ladder/submission-3.py
@@ -0,0 +1,49 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+        def diff_between_two_words(str1, str2):
+            cur_len = 0
+            for i,ch in enumerate(str1):
+                if str1[i] != str2[i]:
+                    cur_len = cur_len+1
+            return cur_len
+
+        def bfs(cur_level):
+            nonlocal q, found
+            if found:
+                return 
+            neighbors = set()
+            if not q:
+                return -1
+
+            for cur_word in q:
+                if diff_between_two_words(cur_word, endWord) == 0:
+                    found = True
+                    return cur_level
+
+                for word in wordList:
+                    if word not in visited:
+                        diff_char = diff_between_two_words(cur_word, word)
+                        if diff_char == 1:
+                            neighbors.add(word)
+                
+            for w in neighbors:
+                visited.add(w)
+            q = list(neighbors)
+
+            ret_val = bfs(cur_level+1)
+            if ret_val and ret_val > 0:
+                return ret_val
+            return -1
+
+
+        q=[]
+        visited = set()
+        found = False
+        q.append(beginWord)
+        visited.add(beginWord)
+        ret = bfs(1)
+        if ret == -1:
+            return 0
+        return ret
