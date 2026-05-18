915edca892e46f42bb08c388492165e9783ce0a9
supreeth0895 <supreeth0895@gmail.com>
2026-05-17 19:46:16 -0700

    Add: coin-change-ii - submission-2

---
+++ Data Structures & Algorithms/coin-change-ii/submission-2.py
@@ -0,0 +1,24 @@
+#SUPREETH - Backtracking + Memoization
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        def backtrack (cur_idx,rem_amt):
+            if (cur_idx,rem_amt) in memo:
+                return memo[(cur_idx,rem_amt)]
+            if cur_idx == len(coins):
+                return 0
+            if rem_amt < 0:
+                return 0
+            if rem_amt == 0:
+                return 1
+            path1 = backtrack(cur_idx, rem_amt - coins[cur_idx])
+            path2 = backtrack(cur_idx+1, rem_amt)
+            memo[(cur_idx,rem_amt)] = path1+path2
+            return path1+path2
+        
+        memo = {}
+        return backtrack(0, amount)
+            
+            
+
+
+        
