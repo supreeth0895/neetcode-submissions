3201031b7820dc493d44c92617a31b94ddf17a2c
supreeth0895 <supreeth0895@gmail.com>
2026-05-20 21:13:10 -0700

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.py
@@ -0,0 +1,15 @@
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.q = []
+        self.k = k
+        for n in nums:
+            self.add(n)
+        
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.q, val)
+        if len(self.q) > self.k:
+            heapq.heappop(self.q)
+        return self.q[0]
+
+#PQ is implemented as a binary Tree where cur node is always > child nodes
