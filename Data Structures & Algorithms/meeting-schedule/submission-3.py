6736551f6fec50108c82c696905fde8b5d218341
supreeth0895 <supreeth0895@gmail.com>
2026-05-23 20:25:46 -0700

    Add: meeting-schedule - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule/submission-3.py
@@ -0,0 +1,36 @@
+#SUPREETH :
+# This is basically MErge two intervals, but from this, you can learn to Sort custom objects objects
+# You get the key on which we need to sort using :
+# def get_start(self, interval):
+#     return interval.start
+
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def get_start(self, interval):
+        return interval.start
+    
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=self.get_start)
+        if len(intervals) == 0:
+            return True
+        cur_interval = intervals[0]
+        end_idx = 1
+        while end_idx < len(intervals):
+            if cur_interval.end > intervals[end_idx].start:
+                return False
+            else:
+                cur_interval = intervals[end_idx]
+                end_idx = end_idx+1
+        return True
+
+
+            
+
+
