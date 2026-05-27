cd385e50a6711dc2482c6f2be2e2669a0068d3d7
supreeth0895 <supreeth0895@gmail.com>
2026-05-27 06:59:56 -0700

    Add: reconstruct-flight-path - submission-4

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-4.py
@@ -0,0 +1,50 @@
+#SUPREETH - dfs
+from typing import List
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        answer = []
+        found_path = False
+
+        my_map = {}
+        edge_count = {}
+        
+        for src, dst in tickets:
+            if src not in my_map:
+                my_map[src] = []
+            my_map[src].append(dst)
+            
+            if (src, dst) not in edge_count:
+                edge_count[(src, dst)] = 0
+            edge_count[(src, dst)] += 1
+
+        for key in my_map:
+            my_map[key].sort()
+
+        def dfs(cur_node, nodes_travelled):
+            nonlocal found_path, answer
+            if len(nodes_travelled) == len(tickets) + 1:
+                answer = nodes_travelled[:]
+                found_path = True
+                return True
+
+            if found_path:
+                return True
+
+            if cur_node in my_map:
+                for neighbor in my_map[cur_node]:
+                    if edge_count.get((cur_node, neighbor), 0) > 0:
+                        nodes_travelled.append(neighbor)
+                        edge_count[(cur_node, neighbor)] -= 1
+
+                        dfs(neighbor, nodes_travelled)
+                        if found_path:
+                            return True
+
+                        nodes_travelled.pop()
+                        edge_count[(cur_node, neighbor)] += 1
+
+            return False
+
+        dfs("JFK", ["JFK"])
+        return answer
