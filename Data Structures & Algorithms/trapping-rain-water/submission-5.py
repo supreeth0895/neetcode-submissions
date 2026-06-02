761c551c6f4bb01847b4465611418987061d6293
supreeth0895 <supreeth0895@gmail.com>
2026-06-02 13:02:52 -0700

    Add: trapping-rain-water - submission-5

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-5.py
@@ -0,0 +1,32 @@
+#SUPREETH
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        max_left_height = [0] * len(height)
+        max_right_height = [0] * len(height)
+
+        #Note: Don't forget this : brute force maximum finding is O(n square)
+        for i in range(0, len(height)):
+            if i == 0:
+                max_left_height[i] = 0
+                continue
+            max_left_height[i] = max(height[i-1], max_left_height[i-1])
+        
+        for i in range(len(height)-1, -1,-1):
+            if i ==len(height)-1 :
+                max_right_height[i] = 0
+                continue
+            max_right_height[i] = max(max_right_height[i+1],height[i+1])
+        
+        amount_of_water_that_can_be_stored = [0]*len(height)
+
+        for i in range(0, len(height)):
+            capacity= min(max_left_height[i], max_right_height[i]) - height[i]
+            if capacity > 0:
+                amount_of_water_that_can_be_stored[i] = capacity
+
+        return sum(amount_of_water_that_can_be_stored)
+
+
+#The Formula that you need to notice here is:
+#Amount of water that will at an index is :
+# Min (Left Max height for an index, Right Max height for an index) - Height of existing wall in that index
