b2548b02d1262d0564a904f6e4b322458859e09e
supreeth0895 <supreeth0895@gmail.com>
2026-06-05 15:52:16 -0700

    Add: cheapest-flight-path - submission-10

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-10.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        adj  = {}
+        for flight in flights:
+            source = flight[0]
+            dest = flight[1]
+            dist = flight[2]
+            if source not in adj:
+                adj[source] = [(dest,dist)]
+            else:
+                adj[source].append((dest,dist))
+        
+        print(adj)
+        min_dist = 100000
+        pq = []
+        heapq.heappush(pq, (0,0,src)) #dist from source, num hops, src node
+
+        while len(pq) != 0:
+            dist, num_hops, node = heapq.heappop(pq)
+            if num_hops > k+1:
+                continue
+            if node == dst and num_hops <= k+1:
+                min_dist = min(min_dist, dist)
+                break
+            if node in adj:
+                for neighbor in adj[node]:
+                    heapq.heappush(pq, (dist+neighbor[1],num_hops+1,neighbor[0]))
+        if min_dist == 100000:
+            return -1
+        return min_dist
+
