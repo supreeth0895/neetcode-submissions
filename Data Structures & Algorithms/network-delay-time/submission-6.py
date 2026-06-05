8c037741f6667590ec68be67fb29b475ba034698
supreeth0895 <supreeth0895@gmail.com>
2026-06-05 03:58:53 -0700

    Add: network-delay-time - submission-6

---
+++ Data Structures & Algorithms/network-delay-time/submission-6.py
@@ -0,0 +1,38 @@
+# SUPREETH -2 : Dijkstras
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        adj = {}
+        for time in times:
+            adj[time[0]] = []
+
+        for time in times:
+            adj[time[0]].append((time[1], time[2])) # adj[source] = (dest,distance)
+        
+        pq = []
+        heapq.heappush(pq, (0,k)) # (dist from source (k), node)
+
+        visited = set()
+
+        max_dist = 0
+
+        while len(pq) != 0:
+            cur_node_dist, cur_node_name = heapq.heappop(pq)
+            if cur_node_name in visited:
+                continue
+            visited.add(cur_node_name)
+            max_dist = max(max_dist,cur_node_dist)
+
+            if len(visited) == n:
+                break
+            
+            if cur_node_name not in adj:
+                continue 
+
+            for new_node_name, new_node_dist in adj[cur_node_name]:
+                dist_from_source = cur_node_dist + new_node_dist
+                heapq.heappush(pq, (dist_from_source, new_node_name))
+        if len(visited) != n:
+            return -1
+        return max_dist
+            
