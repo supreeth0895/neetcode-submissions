966d07148105f10f83dbc6a4e6083269adc0dd1a
supreeth0895 <supreeth0895@gmail.com>
2026-06-06 21:55:57 -0700

    Add: valid-tree - submission-15

---
+++ Data Structures & Algorithms/valid-tree/submission-15.py
@@ -0,0 +1,39 @@
+# In an undirected graph,
+#a cycle exists if you encounter a node that has already been visited,
+#unless that node is the immediate parent (the node you just came from).
+#The original code didn't track the prev node,
+#leading it to mistake the back-and-forth nature of undirected edges for a cycle.
+
+#SUPREETH2
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        adj = {}
+        for i in range(0, n):
+            adj[i] = []
+        for edge in edges:
+            adj[edge[0]].append(edge[1])
+            adj[edge[1]].append(edge[0])
+
+        def dfs(node, prev):
+            if node in visited:
+                return False  # Cycle detected
+            visited.add(node)
+            for val in adj[node]:
+                if val != prev:  # Skip parent to avoid treating undirected back-edge as cycle
+                    if not dfs(val, node):
+                        return False
+            return True
+            
+        visited = set()
+        count_of_disjoint_graphs = 0
+
+        #Do DFS from Node 0. It must visit all nodes without detecting Cycle.
+        ret_val = dfs(0, -1)
+        if ret_val == False:
+            return False
+        
+        # Check for disjoint Graphs
+        if len(visited) != n:
+            return False
+
+        return True
