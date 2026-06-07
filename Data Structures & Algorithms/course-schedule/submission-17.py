c46d49ed4687be9bb9d31c69f502877c26790706
supreeth0895 <supreeth0895@gmail.com>
2026-06-06 20:42:20 -0700

    Add: course-schedule - submission-17

---
+++ Data Structures & Algorithms/course-schedule/submission-17.py
@@ -0,0 +1,36 @@
+#SUPREETH2
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        #BUILD ADJ MATRIX:
+        adj = {}
+        for i in range(0, numCourses):
+            adj[i] = []
+        for p in prerequisites:
+            adj[p[0]].append(p[1])
+        
+        #DFS - With Visiting and Visited Tracking. Visiting, is visited elements in the cur path.
+        # Once current path is over, you can move to Visited.
+        def dfs(course):
+            nonlocal isCycle
+            if course in visiting:
+                isCycle = True
+                return
+            
+            visiting.add(course)
+            for pre_req in adj[course]:
+                dfs(pre_req)
+            visiting.remove(course)
+            # visited.add(course) - Actually we don't care if we visit all nodes. Only if  there is cycle. There might be unconnected graph. So we don't care if we visit all nodes.
+
+            
+
+
+        visited = set()
+        isCycle = False
+        num_of_nodes_without_pre_req = 0
+        for i in range(0, numCourses):
+            visiting = set()
+            dfs(i)
+            if isCycle:
+                return False
+        return True
